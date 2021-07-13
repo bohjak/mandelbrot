b8e71b0009ddeebad9d7fac6b18b105be5221a47
jakub.bohacek <jakub.bohacek@sinnerschrader.com>
2021-07-13 17:43:15 +0200

    add escape_time fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use num_complex::Complex;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn escape_time(c: Complex::<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex{re: 0.0, im: 0.0};
+
+    for i in 0..limit {
+        if z.norm_sqr() > 4. {
+            return Some(i);
+        }
+
+        z = z * z + c;
+    }
+
+    None
+}
+
+#[test]
+fn test_escape_time() {
+    assert_eq!(escape_time(Complex{re: 0.0, im: 0.0}, 100), None);
+    assert_eq!(escape_time(Complex{re: 2.0, im: 2.0}, 10), Some(1));
+    assert_eq!(escape_time(Complex{re: 0.25, im: 0.5}, 100), None);
 }
