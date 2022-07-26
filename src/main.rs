15b885a71c9e5affc2d99510f88d41e022b94047
jakub.bohacek <jamesbohacek@outlook.com>
2022-07-26 19:30:45 +0200

    add exaple window code

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,150 @@ use crossbeam;
 use image::png::PNGEncoder;
 use image::ColorType;
 use num::Complex;
+use pixels::{Error, Pixels, SurfaceTexture};
 use std::fs::File;
 use std::str::FromStr;
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, MouseScrollDelta, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 fn main() {
+    // render_to_file();
+    render_to_window().unwrap();
+}
+
+const WIDTH: u32 = 400;
+const HEIGHT: u32 = 400;
+const BOX_SIZE: i16 = 64;
+
+fn render_to_window() -> Result<(), Error> {
+    let event_loop = EventLoop::new();
+
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        let scaled_size = LogicalSize::new(WIDTH as f64 * 3.0, HEIGHT as f64 * 3.0);
+        WindowBuilder::new()
+            .with_title("Mandelbrot")
+            .with_inner_size(scaled_size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
+
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+    };
+    let mut world = World::new();
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::RedrawRequested(_) => {
+            world.draw(pixels.get_frame());
+            if pixels.render().is_err() {
+                *control_flow = ControlFlow::Exit;
+            }
+        }
+        Event::WindowEvent {
+            event: WindowEvent::Resized(size),
+            ..
+        } => {
+            pixels.resize_surface(size.width, size.height);
+        }
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => {
+            *control_flow = ControlFlow::Exit;
+        }
+        Event::WindowEvent {
+            event:
+                WindowEvent::MouseWheel {
+                    delta: MouseScrollDelta::LineDelta(_, direction),
+                    ..
+                },
+            ..
+        } => {
+            println!("{}", direction);
+            world.update();
+            window.request_redraw();
+        }
+        Event::WindowEvent {
+            event: WindowEvent::KeyboardInput { input, .. },
+            ..
+        } => {
+            match input.virtual_keycode {
+                Some(VirtualKeyCode::Escape) => {
+                    *control_flow = ControlFlow::Exit;
+                }
+                Some(VirtualKeyCode::A) => {
+                    world.update();
+                    window.request_redraw();
+                }
+                _ => (),
+            };
+        }
+        Event::MainEventsCleared => {}
+        _ => (),
+    });
+}
+
+struct World {
+    box_x: i16,
+    box_y: i16,
+    velocity_x: i16,
+    velocity_y: i16,
+}
+
+// TODO: merge with Mandelbrot code
+impl World {
+    fn new() -> Self {
+        Self {
+            box_x: 24,
+            box_y: 16,
+            velocity_x: 1,
+            velocity_y: 1,
+        }
+    }
+
+    fn update(&mut self) {
+        if self.box_x <= 0 || self.box_x + BOX_SIZE > WIDTH as i16 {
+            self.velocity_x *= -1;
+        }
+        if self.box_y <= 0 || self.box_y + BOX_SIZE > HEIGHT as i16 {
+            self.velocity_y *= -1;
+        }
+
+        self.box_x += self.velocity_x;
+        self.box_y += self.velocity_y;
+    }
+
+    fn draw(&self, frame: &mut [u8]) {
+        for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+            let x = (i % WIDTH as usize) as i16;
+            let y = (i / WIDTH as usize) as i16;
+
+            let inside_the_box = x >= self.box_x
+                && x < self.box_x + BOX_SIZE
+                && y >= self.box_y
+                && y < self.box_y + BOX_SIZE;
+
+            let rgba = if inside_the_box {
+                [0x5e, 0x48, 0xe8, 0xff]
+            } else {
+                [0x48, 0xb2, 0xe8, 0xff]
+            };
+
+            pixel.copy_from_slice(&rgba);
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn render_to_file() {
     let args = parse_args();
 
     let mut pixels = vec![0; args.bounds.0 * args.bounds.1];
@@ -186,4 +326,3 @@ fn test_pixel_to_point() {
         }
     );
 }
-
